1ee9b61c9ab182b5dabca47468b60b6a715fa67e
singhhardy <singhhardy724@gmail.com>
2022-03-01 13:14:41 -0800

    initial

---
+++ build/precache-manifest.7f62069e2f45c7927b8f86990975f096.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb90e385e0586cf0a1fd",
+    "url": "/static/css/main.37d85791.chunk.css"
+  },
+  {
+    "revision": "eb90e385e0586cf0a1fd",
+    "url": "/static/js/main.eb90e385.chunk.js"
+  },
+  {
+    "revision": "19aeac00f58c99a84b4e",
+    "url": "/static/js/1.19aeac00.chunk.js"
+  },
+  {
+    "revision": "d9fb7f08d3845cf12c70",
+    "url": "/static/css/2.945fc76c.chunk.css"
+  },
+  {
+    "revision": "d9fb7f08d3845cf12c70",
+    "url": "/static/js/2.d9fb7f08.chunk.js"
+  },
+  {
+    "revision": "6b604fcca756c0b56f34",
+    "url": "/static/js/runtime~main.6b604fcc.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "fb5c1511611489d7939363ee67e7f14d",
+    "url": "/static/media/rewards-money.fb5c1511.png"
+  },
+  {
+    "revision": "e7b69f48ef44239f363cf993801d2968",
+    "url": "/index.html"
+  }
+];
